31fc26b47924d21de10a188e8dafae3ac0ec4297
michael92394 <52435976+michael92394@users.noreply.github.com>
2026-03-04 22:38:05 -0600

    Update RandomPlayerbotMgr.h

--- src/Bot/RandomPlayerbotMgr.h
+++ src/Bot/RandomPlayerbotMgr.h
@@ -206,7 +206,7 @@ public:
 
     // RTG: public wrappers for global event cache (keyed at bot=0)
     uint32 RTG_GetGlobalEvent(std::string const& key) { return GetEventValue(0, key); }
-    void RTG_SetGlobalEvent(std::string const& key, uint32 value, uint32 ttlSeconds, std::string const& data = \"\")
+    void RTG_SetGlobalEvent(std::string const& key, uint32 value, uint32 ttlSeconds, std::string const& data = "")
     {
         SetEventValue(0, key, value, ttlSeconds, data);
     }
