bf5d78ab4f7654886d034f6744f363742f72b1c1
michael92394 <52435976+michael92394@users.noreply.github.com>
2026-03-22 19:44:59 -0500

    Update RandomPlayerbotMgr.cpp

--- src/Bot/RandomPlayerbotMgr.cpp
+++ src/Bot/RandomPlayerbotMgr.cpp
@@ -1666,7 +1666,7 @@ void RandomPlayerbotMgr::UpdateAIInternal(uint32 elapsed, bool /*minimal*/)
                 uint32 teleportSentAt = RTG_GetTeleportSentAt(botId);
                 if (!teleportSentAt)
                 {
-                    uint32 nowTs = uint32(GameTime::GetGameTime());
+                    uint32 nowTs = uint32(GameTime::GetGameTime().count());
 
 					SetEventValue(botId, "rtg_lfg_teleport_sent", nowTs, 60, addData);
                     RTG_RuntimeBreadcrumb(fmt::format("[RTG][RDF][TELEPORT] helper={} owner={} state=dispatch", botId, desiredOwner));
